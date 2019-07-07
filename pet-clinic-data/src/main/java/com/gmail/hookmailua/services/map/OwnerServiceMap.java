c6de3bfb441d83258f2db3a3cc7a0afaab674c01
Alexander Mishchenko <alex.mishchenko@emc.com>
2019-07-07 04:02:15 +0300

    Impl Map based services. Close #6

---
+++ pet-clinic-data/src/main/java/com/gmail/hookmailua/services/map/OwnerServiceMap.java
@@ -0,0 +1,33 @@
+package com.gmail.hookmailua.services.map;
+
+import com.gmail.hookmailua.model.Owner;
+import com.gmail.hookmailua.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.findById(id);
+    }
+}
