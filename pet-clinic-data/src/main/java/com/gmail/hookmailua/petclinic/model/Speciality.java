24d0b89668bc0a54fac92bd88a378a3716c7397b
Alexander Mishchenko <alex.mishchenko@emc.com>
2019-07-26 08:44:01 +0300

    Create Vet Speciality Entity, Associate To Vet. Close #27

---
+++ pet-clinic-data/src/main/java/com/gmail/hookmailua/petclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package com.gmail.hookmailua.petclinic.model;
+
+public class Speciality extends BaseEntity {
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    private String description;
+}
--- pet-clinic-data/src/main/java/com/gmail/hookmailua/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/gmail/hookmailua/petclinic/model/Vet.java
@@ -1,5 +1,16 @@
 package com.gmail.hookmailua.petclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
 
+    private Set<Speciality> specialoties;
+
+    public Set<Speciality> getSpecialoties() {
+        return specialoties;
+    }
+
+    public void setSpecialoties(Set<Speciality> specialoties) {
+        this.specialoties = specialoties;
+    }
 }
