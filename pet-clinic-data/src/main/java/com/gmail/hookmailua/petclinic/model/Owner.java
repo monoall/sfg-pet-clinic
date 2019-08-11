20f04dbadbb424b7c1092984b898cf7effae6efe
Alexander Mishchenko <alex.mishchenko@emc.com>
2019-08-11 08:19:26 +0300

    Update owner JPA entity mappings. Close #36

--- pet-clinic-data/src/main/java/com/gmail/hookmailua/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/gmail/hookmailua/petclinic/model/Owner.java
@@ -1,14 +1,24 @@
 package com.gmail.hookmailua.petclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
 
-    private Set<Pet> pets = new HashSet<>();
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "telephone")
     private String telephone;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
+    private Set<Pet> pets = new HashSet<>();
 
     public String getAddress() {
         return address;
@@ -41,6 +51,4 @@ public class Owner extends Person {
     public void setPets(Set<Pet> pets) {
         this.pets = pets;
     }
-
-
 }
