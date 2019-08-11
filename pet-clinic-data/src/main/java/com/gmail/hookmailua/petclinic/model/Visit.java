fd7858d42e70acb0604edb832834b2b841c31495
Alexander Mishchenko <alex.mishchenko@emc.com>
2019-08-11 08:34:13 +0300

    Added Visit Entity. Close #38

--- pet-clinic-data/src/main/java/com/gmail/hookmailua/petclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/gmail/hookmailua/petclinic/model/Visit.java
@@ -1,10 +1,21 @@
 package com.gmail.hookmailua.petclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
+
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
+    private Pet pet;
 
     public LocalDate getDate() {
         return date;
@@ -29,6 +40,4 @@ public class Visit extends BaseEntity {
     public void setPet(Pet pet) {
         this.pet = pet;
     }
-
-    private Pet pet;
 }
