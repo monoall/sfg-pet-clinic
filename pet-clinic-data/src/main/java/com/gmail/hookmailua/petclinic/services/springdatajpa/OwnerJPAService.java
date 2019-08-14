9ae9f5da2d66d0e702cb2a32c435a02529916cb0
Alexander Mishchenko <alex.mishchenko@emc.com>
2019-08-15 02:38:05 +0300

    Added Spring Data JPA service for owners. Close #40

---
+++ pet-clinic-data/src/main/java/com/gmail/hookmailua/petclinic/services/springdatajpa/OwnerJPAService.java
@@ -0,0 +1,52 @@
+package com.gmail.hookmailua.petclinic.services.springdatajpa;
+
+import com.gmail.hookmailua.petclinic.model.Owner;
+import com.gmail.hookmailua.petclinic.repositories.OwnerRepository;
+import com.gmail.hookmailua.petclinic.services.OwnerService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class OwnerJPAService implements OwnerService {
+    private final OwnerRepository ownerRepository;
+
+    public OwnerJPAService(OwnerRepository ownerRepository) {
+        this.ownerRepository = ownerRepository;
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return ownerRepository.findByLastName(lastName);
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        Set<Owner> owners = new HashSet<>();
+        ownerRepository.findAll().forEach(owners::add);
+        return owners;
+    }
+
+    @Override
+    public Owner findById(Long aLong) {
+        return ownerRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return ownerRepository.save(object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        ownerRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        ownerRepository.deleteById(aLong);
+    }
+}
